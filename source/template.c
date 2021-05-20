5ddee4de79478efa8692126f063e97dae3849a6b
Jonas Jensen <jdudal11@gmail.com>
2021-05-20 23:14:03 +0200

    Almost added player sprite

--- source/template.c
+++ source/template.c
@@ -3,6 +3,7 @@
 #include <tonc_oam.h>
 
 #include "tools.h"
+#include "sprites/player.h"
 
 OBJ_ATTR obj_buffer[128];
 
@@ -11,22 +12,19 @@ int main(void) {
 	
 	REG_BG0CNT = BG_SIZE0 | (0x8 << 8) | BG_8BPP | (0x0 << 2) | 0x0; //Background size = 256x256, screen starts at VRAM + 8 * 2kb, color mode = 256x1, character starts at VRAM + 0 * 16kb, priority = 0
 	
-	u16 testTilePal[16] = {0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF};
-	u32 testTile[8] = {0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF};
-	
 	//Same as memcpy(&tile_mem[4][0], testTile, 16);, but this actually works
-	for (int i = 0; i < 8; i++) {
-		tile_mem[4][0].data[i] = testTile[i];
+	for (int i = 0; i < playerTileSize * 8; i++) {
+		tile_mem[4][0].data[i] = playerTileData[i];
 	}
 	
 	for (int i = 0; i < 16; i++) {
-		pal_obj_mem[i] = testTilePal[i];
+		pal_obj_mem[i] = playerPallet[i];
 	}
 	
 	oam_init(obj_buffer, 128);
 	
 	OBJ_ATTR *test = &obj_buffer[0];
-	obj_set_attr(test, ATTR0_SQUARE, ATTR1_SIZE_64, ATTR2_PALBANK(0) | 0);
+	obj_set_attr(test, ATTR0_SQUARE, ATTR1_SIZE_16, ATTR2_PALBANK(0) | 0);
 	
 	u8 x = 0, y = 0;
 	
